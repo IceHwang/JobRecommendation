235df4519a2fde39e57c6d0e1b7344aa0347581c
Ice <719205998@qq.com>
2019-07-01 13:48:43 +0800

    Bug fixed

--- DataPreProcess/mainTest.py
+++ DataPreProcess/mainTest.py
@@ -3,15 +3,39 @@ import KeywordMapper
 from FileIO import FileIO
 
 
-def main():
+
+def transformTest(fileName,label):
     file = FileIO("../data/")
+
     stopWordList=file.getStopWordList("stopWord.txt")
     similiarWordList=file.getSimiliarWordList("similiarWord.txt")
-    javaJsonStringList=file.getJsonStringList("Java_data.json")
-    jsonList=DataCleanner.DataCleanner.getCleanedJsonList("java",javaJsonStringList,stopWordList,similiarWordList)
+    JsonStringList=file.getJsonStringList(fileName)
+    jsonList=DataCleanner.DataCleanner.getCleanedJsonList(label,JsonStringList,stopWordList,similiarWordList)
     mylist=KeywordMapper.KeyWordMapper.transformJsonList(jsonList)
-    file.saveJsonList("test.json",mylist)
     print(mylist)
+    return mylist
+
+
+def main():
+    file = FileIO("../data/")
+    # similiarWordList= file.getSimiliarWordList("similiarWord.txt")
+    # file.setSimiliarWordList("similiarWord.txt",similiarWordList)
+    # stopWordList = file.getSimiliarWordList("stopWordList.txt")
+    # file.setStopWordList("similiarWord.txt",stopWordList)
+
+    totalList=[]
+    totalList=totalList+transformTest("C_data.json","c")
+    totalList=totalList+transformTest("C++_data.json","c++")
+    totalList=totalList+transformTest("Hadoop_data.json","hadoop")
+    totalList=totalList+transformTest("Java_data.json","java")
+    totalList=totalList+transformTest("PHP_data.json","php")
+    file = FileIO("../data/")
+    file.saveJsonList("test.txt",totalList)
+    return
+
+
+
+
 
 
 if __name__ == '__main__':
