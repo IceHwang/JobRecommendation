f29db4f150d6b0768c9ffff36404b7de32033fcc
Ice <719205998@qq.com>
2019-07-01 19:24:49 +0800

    整合SpringBoot与Spark

---
+++ SpringBootSpark/src/main/java/com/example/Controller/UsersController.java
@@ -0,0 +1,18 @@
+package com.example.Controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UsersController {
+
+//    @ResponseBody
+//    @RequestMapping("/register")
+//    public String register() {
+//        return "hello!";
+//
+//    }
+
+}
